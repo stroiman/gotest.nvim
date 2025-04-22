e4a394c91b47180415d28de0c81970765d42505c
Peter Strøiman <peter@stroiman.com>
2025-04-22 10:33:00 +0200

    Use vim.system to spawn jobs

--- lua/gotest/autogroup.lua
+++ lua/gotest/autogroup.lua
@@ -1,20 +1,19 @@
 -- Add a uuid to the group name, should guarantee no conflicts with other groups
---
-local create_group = function()
-  return vim.api.nvim_create_augroup("go-autotest-fee42c62-0e06-11f0-9e7f-06888228c6fb", { clear = true })
-end
+
+local group_name = "go-autotest-fee42c62-0e06-11f0-9e7f-06888228c6fb"
+local group_id = vim.api.nvim_create_augroup(group_name, { clear = true })
 
 local M = {
-  group = create_group(),
+  group = group_id,
+  group_name = group_name,
 }
 
 M.get_group = function()
   return M.group
 end
 
---- "Unload" all autocommands by recreating the autogroup
 M.unload = function()
-  M.group = create_group()
+  vim.api.nvim_create_augroup(group_name, { clear = true })
 end
 
 return M
--- lua/gotest/autorunner.lua
+++ lua/gotest/autorunner.lua
@@ -4,6 +4,12 @@ local f = require("gotest.functions")
 
 M = {}
 
+local create_buffer = function()
+  local buf = vim.api.nvim_create_buf(false, false)
+  vim.api.nvim_set_option_value("buftype", "nofile", { buf = buf })
+  return buf
+end
+
 M.del_buffer = function()
   if M.buffer and vim.api.nvim_buf_is_valid(M.buffer) then
     vim.api.nvim_buf_delete(M.buffer, { force = true })
@@ -43,10 +49,23 @@ M.show_output = function()
   vim.wo[M.win].wrap = false
 end
 
+vim.api.nvim_create_autocmd("User", {
+  pattern = "GoTestDone",
+  callback = function(ev)
+    local success = ev.data.success
+    if success then
+      status_window.set_status("PASS")
+      M.hide_output()
+    else
+      status_window.set_status("FAIL")
+      M.show_output()
+    end
+  end,
+})
+
 M.store_test_result = function(data, errors)
   if not M.buffer then
-    M.buffer = vim.api.nvim_create_buf(false, false)
-    vim.api.nvim_set_option_value("buftype", "nofile", { buf = M.buffer })
+    M.buffer = create_buffer()
   end
 
   vim.api.nvim_set_option_value("modifiable", true, { buf = M.buffer })
@@ -73,40 +92,68 @@ M.setup = function()
   })
 
   vim.api.nvim_create_autocmd("BufWritePost", {
-    group = autogroup.group,
+    group = autogroup.group_name,
     pattern = "*.go",
     callback = function()
-      local output = {}
       local errors = {}
+      if not M.buffer then
+        M.buffer = create_buffer()
+      end
+      vim.api.nvim_set_option_value("modifiable", true, { buf = M.buffer })
+      vim.api.nvim_buf_set_lines(M.buffer, 0, -1, false, {})
+
       vim.cmd([[messages clear]])
+      vim.api.nvim_exec_autocmds("user", { pattern = "GoTestStart" })
       status_window.set_status("RUNNING")
-      vim.fn.jobstart({ "go", "test", "./...", "-test.short", "-vet=off" }, {
+      local std_out_buffer = ""
+      local std_err_buffer = ""
+      vim.system({ "go", "test", "./...", "-test.short", "-vet=off" }, {
         env = { GOEXPERIMENT = "synctest" },
-        stdout_buffered = true, -- One output line at a time
-        on_stdout = function(_, data)
+        text = true,
+        stdout = function(err, chunk)
+          if not chunk then
+            return
+          end
+          local data = vim.split(chunk, "\n")
+          data[1] = std_out_buffer .. data[1]
+          std_out_buffer = table.remove(data)
+          local output = {}
           for _, line in ipairs(data) do
             if string.find(line, "ok") ~= 1 and string.find(line, "?") ~= 1 then
               table.insert(output, line)
             end
           end
+          vim.schedule(function()
+            vim.api.nvim_buf_set_lines(M.buffer, -1, -1, false, output)
+          end)
         end,
-        on_stderr = function(_, data)
+        stderr = function(_, chunk)
+          if not chunk then
+            return
+          end
+          local data = vim.split(chunk, "\n")
+          data[1] = std_err_buffer .. data[1]
+          std_err_buffer = table.remove(data)
           for _, line in ipairs(data) do
             table.insert(errors, line)
           end
         end,
-        on_exit = function(job_id, exit_code)
-          status_window.open_window()
-          M.store_test_result(output, errors)
-          if exit_code == 0 then
-            status_window.set_status("PASS")
-            M.hide_output()
-          else
-            status_window.set_status("FAIL")
-            M.show_output()
-          end
-        end,
-      })
+      }, function(out)
+        local exit_code = out.code
+        if #errors > 0 then
+          vim.schedule(function()
+            vim.api.nvim_buf_set_lines(M.buffer, 0, 0, false, errors)
+            status_window.open_window()
+            vim.api.nvim_exec_autocmds("user", {
+              pattern = "GoTestDone",
+              data = {
+                exit_code = exit_code,
+                success = exit_code == 0,
+              },
+            })
+          end)
+        end
+      end)
     end,
   })
 end
